68fdec4832916847d892a93f3cad7e6d750f7493
Dominique Goncalves & Zackory Taylor <pair+dgonca.zacktaylor@devbootcamp.com>
2017-08-07 16:48:12 -0500

    require bcrypt and add form partials for questions.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,15 +9,34 @@ class QuestionsController < ApplicationController
   end
 
   def new
+    @question = Question.new
   end
 
   def edit
   end
 
   def create
+    @question = Question.new(question_params)
+
+    respond_to do |format|
+      if @question.save
+        format.html { redirect_to @question, notice: 'Question was successfully saved'}
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   def update
+    respond_to do |format|
+      if @question.update(question_params)
+        format.html { redirect_to @question, notice: 'Question was successfully updated.' }
+        format.json { render :show, status: :ok, location: @question }
+      else
+        format.html { render :edit }
+        format.json { render json: @question.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
