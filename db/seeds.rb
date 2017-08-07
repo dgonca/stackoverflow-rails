8fd98493fb914a2890afc505980dd5e553aa608b
Dominique Goncalves & Zackory Taylor <pair+dgonca.zacktaylor@devbootcamp.com>
2017-08-07 17:47:08 -0500

    matybe?

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # require_relative '../app/models/user'
+require_relative '../config/environment'
 User.create([
   {name: "Heroko", username: "yes", email:"zac@csd", password: "panda" },
   {name: "zack taylor", username: "zack", email:"zack@zack.zack", password: "panda" },
